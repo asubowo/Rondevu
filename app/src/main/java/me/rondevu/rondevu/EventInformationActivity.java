953a0b0b196b00e5554b1450071d9feda310d258
Andrew Subowo <andrew.subowo@gmail.com>
2015-04-12 07:19:01 -0400

    Giving this branch to Sean
    JSON updates

---
+++ app/src/main/java/me/rondevu/rondevu/EventInformationActivity.java
@@ -0,0 +1,53 @@
+package me.rondevu.rondevu;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+public class EventInformationActivity extends ActionBarActivity {
+
+
+    private String eventInformation;
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_information);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_event_information, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/me/rondevu/rondevu/MainActivity.java
+++ app/src/main/java/me/rondevu/rondevu/MainActivity.java
@@ -1,34 +1,44 @@
 package me.rondevu.rondevu;
 
 import android.content.Intent;
-import android.net.NetworkInfo;
+
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.Toast;
 
+
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-
-import static android.net.NetworkInfo.State.CONNECTED;
-import static android.net.NetworkInfo.State.DISCONNECTED;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -59,7 +69,27 @@ public class MainActivity extends ActionBarActivity {
         ea = new EventAdapter(createList(0));
         recList.setAdapter(ea);
 
+
+        recList.addOnItemTouchListener(
+                new RecyclerItemClickListener(this.getApplicationContext(), new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                        Event event = ea.getEvent(position);
+
+                        Intent intent = new Intent(getApplication(), EventInformationActivity.class);
+                        intent.putExtra("eventPass", event.toString());
+
+                        startActivity(intent);
+                    }
+                }));
         testAdd();
+
+        try {
+            refreshFeed();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -99,12 +129,18 @@ public class MainActivity extends ActionBarActivity {
         }
 
         if (id == R.id.action_refresh) {
-            ea.getList().add(other);
+             ea.getList().add(other);
+
+
+            try {
+                refreshFeed();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
             ea.notifyDataSetChanged();
             llm.setSmoothScrollbarEnabled(true);
             llm.scrollToPosition(0);
-
-            new RequestTask().execute("http://stackoverflow.com");
 
         }
 
@@ -117,51 +153,50 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    class RequestTask extends AsyncTask<String, String, String> {
+    public void refreshFeed() throws JSONException {
+        Log.d("me.rondevu.rondevu", "refreshing feed");
 
-        @Override
-        protected String doInBackground(String... uri) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpResponse response;
-            String responseString = null;
+        DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+        HttpPost httppost = new HttpPost("http://default-environment-t33qajk2jx.elasticbeanstalk.com/api/events");
+// Depends on your web service
+        httppost.setHeader("Content-type", "application/json");
 
-            try {
-                response = httpclient.execute(new HttpGet(uri[0]));
-                StatusLine statusLine = response.getStatusLine();
-                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    response.getEntity().writeTo(out);
-                    responseString = out.toString();
-                    out.close();
-                } else{
-                    //Closes the connection.
-                    response.getEntity().getContent().close();
-                    throw new IOException(statusLine.getReasonPhrase());
-                }
-            } catch (ClientProtocolException e) {
-                //TODO Handle problems..
-            } catch (IOException e) {
-                //TODO Handle problems..
+        InputStream inputStream = null;
+        String result = null;
+        try {
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+
+            inputStream = entity.getContent();
+            // json is UTF-8 by default
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
             }
-            return responseString;
-        }
+            result = sb.toString();
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            //Do anything with response..
+            Log.d("me.rondevu.rondevu", result);
 
+        } catch (Exception e) {
+
+        } finally {
+            try {
+                if (inputStream != null) inputStream.close();
+            } catch (Exception squish) {
+            }
         }
     }
-
-
 
     private List<Event> createList(int size) {
 
         List<Event> result = new ArrayList<Event>();
-        for (int i=1; i <= size; i++) {
+        for (int i = 1; i <= size; i++) {
 
         }
         return result;
     }
+
 }
