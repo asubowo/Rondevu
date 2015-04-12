e4beb8b78365308566204b5c07e7caf04ae0cc77
Andrew Subowo <andrew.subowo@gmail.com>
2015-04-12 10:12:01 -0400

    Seans changes
    seans changes

--- app/src/main/java/me/rondevu/rondevu/MainActivity.java
+++ app/src/main/java/me/rondevu/rondevu/MainActivity.java
@@ -2,7 +2,9 @@ package me.rondevu.rondevu;
 
 import android.content.Intent;
 
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,17 +16,11 @@ import android.view.MenuItem;
 import android.view.View;
 
 import android.widget.Button;
-import android.widget.Toast;
-
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
+
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.json.JSONArray;
@@ -32,8 +28,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
@@ -58,6 +52,10 @@ public class MainActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         createEventButton = (Button) findViewById(R.id.host);
 
         recList = (RecyclerView) findViewById(R.id.cardList);
@@ -78,18 +76,20 @@ public class MainActivity extends ActionBarActivity {
                         Event event = ea.getEvent(position);
 
                         Intent intent = new Intent(getApplication(), EventInformationActivity.class);
-                        intent.putExtra("eventPass", event.toString());
+                        intent.putExtra("eventName", event.getEventName());
+                        intent.putExtra("hostName", event.getHost());
+                        intent.putExtra("info", event.getInfo());
+                        intent.putExtra("location", event.getLocation());
+                        intent.putExtra("category", event.getCategory());
 
                         startActivity(intent);
                     }
                 }));
         testAdd();
 
-        try {
-            refreshFeed();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+
+        new RetrieveFeedTask().execute();
+
     }
 
 
@@ -129,14 +129,9 @@ public class MainActivity extends ActionBarActivity {
         }
 
         if (id == R.id.action_refresh) {
-             ea.getList().add(other);
+            ea.getList().add(other);
 
-
-            try {
-                refreshFeed();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            new RetrieveFeedTask().execute();
 
             ea.notifyDataSetChanged();
             llm.setSmoothScrollbarEnabled(true);
@@ -146,6 +141,24 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, Event> {
+        protected Event doInBackground(Void... arg0) {
+            try {
+                refreshFeed();
+            } catch (JSONException e) {
+                Log.d("me.rondevu.rondevu", "Exception caught in async");
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Void result) {
+
+        }
+    }
+
 
     public void createEvent(View view) {
         Intent intent = new Intent(this, CreateEventActivity.class);
@@ -157,7 +170,11 @@ public class MainActivity extends ActionBarActivity {
         Log.d("me.rondevu.rondevu", "refreshing feed");
 
         DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
-        HttpPost httppost = new HttpPost("http://default-environment-t33qajk2jx.elasticbeanstalk.com/api/events");
+        //HttpPost httppost = new HttpPost("http://api.androidhive.info/contacts/");
+        // URI myuri = new URI("http://default-environment-t33qajk2jx.elasticbeanstalk.com/api/events");
+        Uri myuri = Uri.parse("http://default-environment-t33qajk2jx.elasticbeanstalk.com/api/events");
+        HttpGet httppost = new HttpGet(String.valueOf(myuri));
+        Log.d("me.rondevu.rondevu", "Website " + httppost.getURI().toString());
 // Depends on your web service
         httppost.setHeader("Content-type", "application/json");
 
@@ -178,10 +195,49 @@ public class MainActivity extends ActionBarActivity {
             }
             result = sb.toString();
 
-            Log.d("me.rondevu.rondevu", result);
+            JSONObject jObject = new JSONObject(result);
+
+            Log.d("me.rondevu.rondevu", jObject.toString());
+
+            JSONArray jArray = jObject.getJSONArray("events");
+
+
+            for (int i = 0; i < jArray.length(); i++) {
+                try {
+
+                    
+
+                    JSONObject oneObject = jArray.getJSONObject(i);
+                    // Pulling items from the array
+                    String oneObjectsItem = oneObject.getString("title");
+                    String host = oneObject.getString("host");
+                    String location = oneObject.getString("location");
+                    String info = oneObject.getString("description");
+                    int capacity = oneObject.getInt("capacity");
+                    String category = oneObject.getString("category");
+
+                    Event event = new Event(oneObjectsItem, host, location, info, category, capacity);
+
+                    Log.d("me.rondevu.rondevu", event.toString());
+
+                    ea.getList().add(event);
+                    ea.notifyDataSetChanged();
+                    llm.setSmoothScrollbarEnabled(true);
+                    llm.scrollToPosition(0);
+
+                    Log.d("me.rondevu.rondevu", oneObjectsItem);
+                } catch (JSONException e) {
+                    // Oops
+                }
+            }
+
+
+            Log.d("me.rondevu.rondevu", result + "WE'VE MADE THE TRY!!!");
+            // Log.d("me.rondevu.rondevu", aString);
 
         } catch (Exception e) {
-
+            Log.d("me.rondevu.rondevu", "EXCEPTION CAUGHT HERE");
+            e.printStackTrace();
         } finally {
             try {
                 if (inputStream != null) inputStream.close();
